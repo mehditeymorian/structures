38070ef490f45d22acf723d0cf79b63e7f77e703
Mehdi Teymorian <mehditeymorian322@gmail.com>
2022-11-20 12:13:20 +0330

    feat: add linkedlist example

---
+++ example/list/linked_list/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mehditeymorian/structures/list"
+)
+
+func main() {
+	ll := list.NewLinkedList[int]()
+	ll.Add(1, 2, 3, 4, 5, 6)
+	fmt.Println(ll.GetAll())
+	ll.Remove(0)
+	fmt.Println(ll.GetAll())
+	ll.Remove(4)
+	fmt.Println(ll.GetAll())
+	ll.Remove(2)
+	fmt.Println(ll.GetAll())
+}
